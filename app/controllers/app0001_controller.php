97dda5cd6d3fb819c86d1746705f06c5bb5a19f3
fukunagawataru <fukunagawataru@wataru-no-ibook-g4.local>
2010-07-21 23:36:04 +0900

    add few files

---
+++ app/controllers/app0001_controller.php
@@ -0,0 +1,37 @@
+<?php
+class App0001Controller extends AppController
+{
+	var $name = 'App0001';
+
+	/*
+	 * 初期表示画面
+	 * ユーザーの現在ステージを判定して表示
+	 */
+	function top() {
+		
+	}
+
+	/*
+	 * ランキング画面
+	 */
+	function rank() {
+	
+	}
+
+	/*
+	 * 復習画面
+	 */
+	function review() {
+	
+	}
+
+	/*
+	 * 再挑戦画面
+	 * @param $stage
+	 */
+	function challenge($stage=null) {
+		
+	}
+	
+}
+?>
